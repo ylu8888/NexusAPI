f0634c556b2e1afa618baaee732284f7758cfb3e
ylu8888 <yang.lu.3@stonybrook.edu>
2023-09-01 20:48:45 -0400

    added edit/delete buttons

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom"
 const Product = ({product}) => {
     return (
         <div className="bg-white rounded shadow-lg overflow-hidden">
@@ -6,7 +7,15 @@ const Product = ({product}) => {
                 <h2 className="text font-semibold">{product.name}</h2>
                 <div className="text-sm">Description: {product.description}</div>
                 <div className="text-sm">Year: {product.date}</div>
+
+                <div className="mt-2 flex gap-4">
+                <Link to={`/edit`} className="inline-block w-full text-center shadow-md text-sm bg-gray-900 text-white rounded-sm px-4 py-1 font-bold hover:bg-gray-600 hover:cursor-pointer"> Edit</Link>
+                <Link to={`/edit`} className="inline-block w-full text-center shadow-md text-sm bg-gray-900 text-white rounded-sm px-4 py-1 font-bold hover:bg-gray-600 hover:cursor-pointer"> Delete</Link>
             </div>
+
+            </div>
+
+           
             
         </div>
     )

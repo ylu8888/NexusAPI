dfb7a8016296cb63de9d6d533be615bb82c31204
ylu8888 <yang.lu.3@stonybrook.edu>
2023-09-07 21:19:40 -0400

    changed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,12 @@ const App = () => {
     <nav className="bg-neutral-900 ">
 
     
-      <div className="text-white container w-3/5 mx-auto flex justify-between items-center h-10">
+      <div className="text-white container w-3/5 mx-auto flex justify-between items-center h-10 ">
        
-          <Link to="/"><h2 className="text-white text-sm ">Home</h2></Link>
-          <a href="/https://odysseymuseum.netlify.app/#about">About</a> 
-          <Link to="/create"><h2 className="text-white text-sm ">Contribute</h2></Link>
-          <a href="mailto:yanglu91603@gmail.com"><h2 className="text-white text-sm">Contact</h2></a>
+          <Link to="/"><h2 className="text-white text-sm hover:text-orange-400 items-center rounded-2xl font-mono">Home</h2></Link>
+          <a href="/https://odysseymuseum.netlify.app/#about" className="text-sm rounded-2xl hover:text-orange-400 font-mono">About</a> 
+          <Link to="/create"><h2 className="text-white text-sm rounded-2xl hover:text-orange-400 font-mono">Contribute</h2></Link>
+          <a href="mailto:yanglu91603@gmail.com"><h2 className="text-white text-sm rounded-2xl hover:text-orange-400 font-mono">Contact</h2></a>
           
 
       </div>
